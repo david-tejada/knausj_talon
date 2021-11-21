dea17c6c9bac6b9d75158a19aea8f29650076f65
David Tejada <davidxtejada@gmail.com>
2021-11-21 20:22:53 +0100

    change letter

--- code/keys.py
+++ code/keys.py
@@ -3,7 +3,7 @@ from typing import Set
 from talon import Module, Context, actions, app
 import sys
 
-default_alphabet = "air bat cap drum each fine gust harp ivy jane kiss look made near ox pit quench rip sun trap urge vest whale plex yank zip".split(
+default_alphabet = "air bat cap drum each fine gust harp inch jane kiss look made near ox pit quench rip sun trap urge vest whale plex yank zip".split(
     " "
 )
 letters_string = "abcdefghijklmnopqrstuvwxyz"
