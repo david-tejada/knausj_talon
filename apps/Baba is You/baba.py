84e83ab9623f2f2a47ff428e9b931b067bad7182
David Tejada <davidxtejada@gmail.com>
2023-07-25 18:06:11 +0200

    Add Baba is You game

---
+++ apps/Baba is You/baba.py
@@ -0,0 +1,6 @@
+from talon import Context, Module, actions, app
+
+ctx = Context()
+mod = Module()
+
+mod.apps.baba = "app.bundle: Baba Is You"
---
+++ apps/Baba is You/baba.talon
@@ -0,0 +1,5 @@
+app: baba
+-
+settings():
+  key_hold = 50.0
+  key_wait = 10.0
